d874c2c00d57c9422ad2a5ccbdf281663bf46629
Souluigi <marcos2003mlr@gmail.com>
2025-09-29 22:08:24 -0300

    🚧 feat: adiciona dois cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:expenses/transaction.dart';
 import 'package:flutter/material.dart';
 
-main() => runApp(MyHomePage());
+main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
   @override
@@ -9,14 +10,45 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+        id: 't1',
+        title: 'Novo Tênis de Corrida',
+        value: 310.76,
+        date: DateTime.now(),
+        ),
+    Transaction(
+        id: 't2',
+        title: 'Conta de Luz',
+        value: 211.30,
+        date: DateTime.now(),
+    ),
+];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Despesas Pessoais'),
         ),
-      body: Center(
-        child: Text('Versão Inicial'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children:<Widget>[
+          Container(
+            child: Card(
+              color: Colors.orange,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children: _transactions.map((tr){
+              return Card(
+                child: Text(tr.title),
+              );
+            }).toList(),
+          )
+        ]
       )
     );
   }
